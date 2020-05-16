e4d00e17982fbc1c5c667b125d8f08496db98045
Obo Agboghidi <oboagboghidi@Obos-MacBook-Pro-2.local>
2020-05-15 20:35:32 -0400

    added basic CSS

--- src/registration.js
+++ src/registration.js
@@ -7,32 +7,97 @@ import {
     Button,
     Paper
 } from "@material-ui/core";
-import styles from "./formStyles.js";
+//import styles from "./formStyles.js";
+import './registration.css';
 
 
 const Registration = (props) => {
     const [ firstName, setFirstName ] = useState("Tim");
-    const [ lastName, setLastName ] = useState("");
+    const [ lastName, setLastName ] = useState("Stark");
     const [ NPI, setNPI ] = useState(0);
-    const [ buisnessAddress, setBuisnessAddress ] = useState("");
-    const [ telePhone, setTelePhone ] = useState("");
-    const [ emailAddress, setEmailAddress ] = useState("")
+    const [ buisnessAddress, setBuisnessAddress ] = useState("4870 N Deer St");
+    const [ telePhone, setTelePhone ] = useState("9514850360");
+    const [ emailAddress, setEmailAddress ] = useState("conway299@gmail.com")
 
-    function doSomething() { // 1. props is accessible inside the function so you can skip the parameter
-        console.log(firstName);
+    const printForm = (e) => { // 1. props is accessible inside the function so you can skip the parameter
+        //e.preventDefault();
+        console.log("It clicked")
+        alert(`Name: ${firstName} ${lastName}${'\n'}NPI: ${NPI}${'\n'}Buisness Address: ${buisnessAddress}${'\n'}TelePhone: ${telePhone}${'\n'}Email: ${emailAddress}${'\n'}`)
      }
-    doSomething();
     
     return (
         <div>
-            <form  >
-                <Typography >
+            <form  onSubmit={() => printForm()} className={"container"}>
+                <Typography className={"formHeader"}>
                     Provider Registration
                 </Typography>
+                <Paper >
+                    <FormGroup className={"formRow"}>
+                        <TextField className={"formField"}
+                            required
+                            type="text"
+                            variant="outlined"
+                            label="First Name"
+                            value={firstName}
+                            onChange={e => setFirstName(e.target.value)}
+                        />
+                        <TextField className={"formField"}
+                            required
+                            type="text"
+                            variant="outlined"
+                            label="Last Name"
+                            value={lastName}
+                            onChange={e => setLastName(e.target.value)}
+                        />
+                        <TextField className={"formField"}
+                            required
+                            type="text"
+                            variant="outlined"
+                            label="NPI"
+                            value={NPI}
+                            onChange={e => setNPI(e.target.value)}
+                        />
+                        <TextField className={"formField"}
+                            required
+                            type="text"
+                            variant="outlined"
+                            label="Buisness Address"
+                            value={buisnessAddress}
+                            onChange={e => setBuisnessAddress(e.target.value)}
+                        />
+                        <TextField className={"formField"}
+                            required
+                            type="text"
+                            variant="outlined"
+                            label="Telephone"
+                            value={telePhone}
+                            onChange={e => setTelePhone(e.target.value)}
+                        />
+                        <TextField className={"formField"}
+                            required
+                            type="text"
+                            variant="outlined"
+                            label="Email"
+                            value={emailAddress}
+                            onChange={e => setEmailAddress(e.target.value)}
+                        />
+                    </FormGroup>
+
+                    <FormGroup className={"formGroup2"}>
+                            <Button
+                                type="submit"
+                                color="primary"
+                                variant="contained"
+                                className={"submitButton"}
+                            >
+                                Submit
+                            </Button>
+                        </FormGroup>
+                    </Paper>
             </form>
         </div>
     )
 
 } 
 
-export default withStyles(styles, { withTheme: true })(Registration);
+export default Registration;
